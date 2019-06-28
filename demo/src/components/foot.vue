b4d90047ef6ac3f399037d957e1cc89fff48ec3a
wenxiaoxiao <wenxiaoxiao22400@163.com>
2019-06-28 15:00:14 +0800

    wentao first

---
+++ demo/src/components/foot.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="foot">
+    <div>
+      jio
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+@import '~style/index.less';
+.foot{
+    position: fixed;
+    bottom: 0;
+    left: 0;
+  
+  .h(150);
+  width: 100%;
+  background: @bg-color-black;
+  .l_h(150);
+  div{
+    color: @font-color-white;
+    .f_s(44);
+  }
+}
+</style>
